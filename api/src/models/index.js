9ad46b8aafa7031aa6ebf1ad747c1cb3aa914c1b
Dang Quang Nguyen <20668533@students.latrobe.edu.au>
2022-10-03 16:07:15 +1100

    lab7

---
+++ api/src/models/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const Sequelize = require('sequelize').default;
+
+// Load our database configuration
+const dbConfig = require('../config/database');
+
+// Connect Sequelize to the database
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, dbConfig);
+
+// Load all of our model definitions
+const models = {
+  Post: sequelize.import(require.resolve('./post'))
+};
+
+// Store the database connection
+models.database = sequelize;
+
+// Export our model definitions
+module.exports = models;
